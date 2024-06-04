a77c7899a30485ec0fef8f747c14a95621eaae10
Nameless1004 <97615919+Nameless1004@users.noreply.github.com>
2024-06-04 23:54:44 +0900

    [level 2] Title: 피보나치 수, Time: 1.00 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -0,0 +1,17 @@
+public class Solution {
+   public int solution(int n)
+  {
+        var a = 0;
+        var b = 1;
+        var sum = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            sum = (a + b) % 1234567;
+            a = b;
+            b = sum;
+        }
+
+        return sum;
+  }
+
+}
