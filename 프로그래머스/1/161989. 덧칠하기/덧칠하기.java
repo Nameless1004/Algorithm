36bc08f5bf969fc1c9c790f7bd933f4545aad9e1
Nameless1004 <97615919+Nameless1004@users.noreply.github.com>
2024-07-16 17:15:03 +0900

    [level 1] Title: 덧칠하기, Time: 2.35 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int start = section[0];
+        int answer = 1;
+        for(int i = 1; i < section.length; ++i){
+            if(section[i] - start >= m ) {
+                answer+=1;
+                start = section[i];
+            }
+        }
+        return answer;
+    }   
+}
